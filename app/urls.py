0c5cf0ab1f2d4cf02e3b13ef6ac5f13cf474fa9f
aguiarjv <jvictoraguiar@live.com>
2024-01-09 16:49:55 -0300

    added cars_list and new_car as class based views

--- app/urls.py
+++ app/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import cars_view
-from cars.views import new_car_view
+from cars.views import NewCarView, CarsView
+from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("cars/", cars_view, name="cars_list"),
-    path("new_car/", new_car_view, name="new_car"),
+    path("cars/", CarsView.as_view(), name="cars_list"),
+    path("new_car/", NewCarView.as_view(), name="new_car"),
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
