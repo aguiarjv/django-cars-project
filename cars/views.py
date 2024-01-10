aac773fe91ef16eac627a1f15535a4878a4575c7
aguiarjv <jvictoraguiar@live.com>
2024-01-10 15:42:09 -0300

    altered views to ListView and CreateView

--- cars/views.py
+++ cars/views.py
@@ -2,31 +2,33 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView, CreateView, DetailView
 
 # Create your views here.
 
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, "new_car.html", {"new_car_form": new_car_form})
+class CarsListView(ListView):
+    model = Car
+    template_name = "cars.html"
+    context_object_name = "cars"
 
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect("cars_list")
-
-        return render(request, "new_car.html", {"new_car_form": new_car_form})
-
-
-class CarsView(View):
-    def get(self, request):
-        cars = Car.objects.all().order_by("model")
-        search = request.GET.get("search")
+    def get_queryset(self):
+        queryset = super().get_queryset().order_by("model")
+        search = self.request.GET.get("search")
 
         if search:
-            cars = cars.filter(model__icontains=search)
+            queryset = queryset.filter(model__icontains=search)
 
-        return render(request, "cars.html", {"cars": cars})
+        return queryset
+
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = "new_car.html"
+    success_url = "/cars/"
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "car_detail.html"
